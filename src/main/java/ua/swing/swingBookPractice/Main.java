3625b799eb7dc6a6c08077e2d6841dc00ea7df82
Serhii <serhii.mask@gmail.com>
2023-06-28 19:47:10 +0300

    feat: add swing main

---
+++ src/main/java/ua/swing/swingBookPractice/Main.java
@@ -0,0 +1,4 @@
+package ua.swing.swingBookPractice;
+
+public class Main {
+}
---
+++ src/main/java/ua/swing/swingVideoPractice/Main.java
@@ -0,0 +1,33 @@
+package ua.swing.swingVideoPractice;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //Створюємо вікно
+        JFrame jFrame = new JFrame(){};
+
+        //Робимо вікно видиме
+        jFrame.setVisible(true);
+
+        //Даємо можливість закривати програму
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Задаємо розмір вікна програми
+    //    jFrame.setSize(500, 300);
+
+        //Задаємо місце появи вікна програми
+    //    jFrame.setLocation(500, 300);
+
+        //Дізнаємо ширину та висону екрану пристрою
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+
+        //Задаємо розмір вікна програми та місце появи вікна програми
+        jFrame.setBounds(dimension.width/2 - 250, dimension.height/2 - 150, 500, 300);
+
+        //Задаємо назву програмі
+        jFrame.setTitle("This my app");
+    }
+}
