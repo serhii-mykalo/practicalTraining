053f4c699c7d85630b2ff11132d15cef8a225eda
Serhii <serhii.mask@gmail.com>
2023-06-29 18:57:01 +0300

    feat: add Swing4EventHandler

---
+++ src/main/java/ua/swing/swingVideoPractice/Swing4EventHandler.java
@@ -0,0 +1,34 @@
+package ua.swing.swingVideoPractice;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.EventHandler;
+
+public class Swing4EventHandler {
+    public static void main(String[] args) {
+        JFrame jFrame = getFrame();
+        JPanel jPanel = new JPanel();
+        jFrame.add(jPanel);
+        JButton jButton = new JButton("submit");
+        jPanel.add(jButton);
+//        jButton.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                jFrame.setTitle(((JButton)e.getSource()).getText());
+//            }
+//        });
+        jButton.addActionListener(EventHandler.create(ActionListener.class, jFrame, "title", "source.text"));
+    }
+
+    static JFrame getFrame() {
+        JFrame jFrame = new JFrame(){};
+        jFrame.setVisible(true);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        jFrame.setBounds(dimension.width/2 - 250, dimension.height/2 - 150, 300, 300);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        return jFrame;
+    }
+}
