3ef55909bca6910e252a36f0d606998c79f58260
Serhii <serhii.mask@gmail.com>
2023-06-29 19:19:22 +0300

    feat: add Swing5LookAndFeel

---
+++ src/main/java/ua/swing/swingVideoPractice/Swing5LookAndFeel.java
@@ -0,0 +1,94 @@
+package ua.swing.swingVideoPractice;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Swing5LookAndFeel {
+    public static void main(String[] args) {
+        JFrame jFrame = getFrame();
+        JPanel jPanel = new JPanel();
+        jFrame.add(jPanel);
+        JButton jButton1 = new JButton("Metal");
+        JButton jButton2 = new JButton("Nimbus");
+        JButton jButton3 = new JButton("CDE/Motif");
+        JButton jButton4 = new JButton("Windows");
+        JButton jButton5 = new JButton("Windows Classic");
+        jPanel.add(jButton1);
+        jPanel.add(jButton2);
+        jPanel.add(jButton3);
+        jPanel.add(jButton4);
+        jPanel.add(jButton5);
+        jButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        jButton2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        jButton3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        jButton4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        jButton5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel");
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+//        UIManager.LookAndFeelInfo[] lookAndFeelInfos = UIManager.getInstalledLookAndFeels();
+//        for (UIManager.LookAndFeelInfo lookAndFeelInfo : lookAndFeelInfos) {
+//            System.out.println(lookAndFeelInfo.getName());
+//            System.out.println(lookAndFeelInfo.getClassName());
+//        }
+    }
+
+    static JFrame getFrame() {
+        JFrame jFrame = new JFrame(){};
+        jFrame.setVisible(true);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        jFrame.setBounds(dimension.width/2 - 250, dimension.height/2 - 150, 500, 500);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        return jFrame;
+    }
+}
